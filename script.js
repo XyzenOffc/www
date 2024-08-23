b44acd328c1d4fc323cdcadcf832fc3064b3e4f8
XyzenOffc <168958615+XyzenOffc@users.noreply.github.com>
2024-08-24 00:01:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.style.display = (menu.style.display === 'block' ? 'none' : 'block');
+});
+
+
+
+
